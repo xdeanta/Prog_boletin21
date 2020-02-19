6ceb36d49eafc9a4ef4f02eb45a88fb83e04714a
Xavier De Anta <xdeantabelisario@danielcastelao.org>
2020-02-19 09:37:23 +0100

    Boletin Modificado

--- biblioteca/Biblioteca.java
+++ biblioteca/Biblioteca.java
@@ -5,10 +5,56 @@
  */
 package biblioteca;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  *
  * @author xdeantabelisario
  */
 public class Biblioteca {
+    private ArrayList<Libro> libreria;
+
+    public Biblioteca() {
+        libreria=new ArrayList<Libro>();
+    }
     
+    public void insertarLibro(Libro l){
+        Libro aux;
+        int index, unidades;
+        Iterator<Libro> it=libreria.iterator();
+        if(libreria.isEmpty()){
+            libreria.add(l);
+        }else{
+            while(it.hasNext()){
+                aux=it.next();
+                if(aux.getISBN().equals(l.getISBN())){
+                    index=libreria.indexOf(aux);
+                    unidades=aux.getUnidades();
+                    unidades++;
+                    aux.setUnidades(unidades);
+                }else{
+                    libreria.add(l);
+                }
+            }
+        }
+    }
+    
+    public void venderLibro(Libro l) throws ExceptionEmpty{
+        if(libreria.isEmpty()){
+            throw new ExceptionEmpty("La biblioteca esta vacia");
+        }
+    }
+    
+    public void buscarLibro(Libro l){
+        
+    }
+    
+    public void mostrarBiblioteca(){
+        
+    }
+    
+    public void purgar(){
+        
+    }
 }
--- biblioteca/Libro.java
+++ biblioteca/Libro.java
@@ -10,5 +10,61 @@ package biblioteca;
  * @author xdeantabelisario
  */
 public class Libro {
-    
+    private String titulo;
+    private String autor;
+    private String ISBN;
+    private float precio;
+    private int unidades;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public int getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(int unidades) {
+        this.unidades = unidades;
+    }
+
+    public Libro(String titulo, String autor, String ISBN, float precio, int unidades) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ISBN = ISBN;
+        this.precio = precio;
+        this.unidades = unidades;
+    }
+
+    public Libro() {
+        this(null,null,null,0,0);
+    }
 }
